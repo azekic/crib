9b14fa6a3cbe59ed34e95ddbf9222db5f469e0c6
Andre Zekic <andre@andres-mbp.lan>
2020-07-10 13:52:07 -0400

    Added placeholders for auth, posts, and comments

---
+++ frontend/src/pages/Comments.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class CommentsPage extends Component {
+    render() {
+        return (<h1>The Comments Page</h1>);
+    }
+}
+
+export default CommentsPage;
---
+++ frontend/src/pages/Posts.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PostsPage extends Component {
+    render() {
+        return (<h1>The Posts Page</h1>);
+    }
+}
+
+export default PostsPage;
