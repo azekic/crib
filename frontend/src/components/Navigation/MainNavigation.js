81fcb0d571cb8e91bfc1c24dd61c4dd7155c40f9
Andre Zekic <andre@Andres-MacBook-Pro.local>
2020-07-12 21:10:53 -0400

    added logic for token to update the UI

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
+
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-    <header className="main-navigation">
-        <div className="main-navigation__logo">
-            <h1>Condo Chat</h1>
-        </div>
-        <nav className="main-navigation__items">
-            <ul>
-                <li>
-                    <NavLink to="/auth">Authenticate</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/posts">Posts</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/comments">Comments</NavLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <AuthContext.Consumer>
+        {context => {
+            return (
+                <header className="main-navigation">
+                    <div className="main-navigation__logo">
+                        <h1>Condo Chat</h1>
+                    </div>
+                    <nav className="main-navigation__items">
+                        <ul>
+                            {!context.token && (
+                            <li>
+                                <NavLink to="/auth">Authenticate</NavLink>
+                            </li>
+                            )}
+                            {context.token && (
+                            <li>
+                                <NavLink to="/posts">Posts</NavLink>
+                            </li>
+                            )}
+                        </ul>
+                    </nav>
+                </header>
+            );
+        }}
+    
+    </AuthContext.Consumer>
 );
 export default mainNavigation;
